d02528d5c51797644ae62f589ab446f0ec8b3e4d
riskalaluna <riskalaluna@gmail.com>
2023-03-31 11:22:35 +0700

    langkah 2,3,4

--- Quicksort_176/Quicksort_176.cpp
+++ Quicksort_176/Quicksort_176.cpp
@@ -43,4 +43,11 @@ void q_short(int low, int high)
 	if (low > high) //Langkah 1
 		return;
 
+	//partition the list into two parts
+	//one containing element less that or equal to pivot
+	//outher containing element greather than pivot
+	i = low + 1; //langkah 3
+	j = high; //langkah 4
+	pivot = arr[low]; //langkah 2
+
 }
