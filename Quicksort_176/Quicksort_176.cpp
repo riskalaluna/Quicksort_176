d20d6ce0e8b052b155df84dd9ace321a358cd22a
riskalaluna <riskalaluna@gmail.com>
2023-03-31 11:03:17 +0700

    masukkan panjang element array

--- Quicksort_176/Quicksort_176.cpp
+++ Quicksort_176/Quicksort_176.cpp
@@ -5,3 +5,16 @@ using namespace std;
 int arr[20];
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of movement
+int n;
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+
+	}
+}
