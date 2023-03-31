4141aec2184dbbdfd9b7e97a8c11cbab94abf88c
riskalaluna <riskalaluna@gmail.com>
2023-03-31 11:37:47 +0700

    langkah 11

--- Quicksort_176/Quicksort_176.cpp
+++ Quicksort_176/Quicksort_176.cpp
@@ -62,7 +62,16 @@ void q_short(int low, int high)
 		//search for an element less thanor equal to pivot
 		while ((arr[j] > pivot) && (j >= low)) //langkah 7
 		{j--; //langkah 8
-
-		}
+		cmp_count++;
+	}
+	cmp_count++;
+	if (i < j) //langkah 9
+		//if greator element is on left of the element
+	{
+		//swap the element at index i with the element at index j
+		swap(i, j);
+		mov_count++;
 	}
 }
+//j now containt the index of the last element in the sort list
+	if (low < j) //langkah 11
